4622effe0ebc36c186f4634afc8937e478fa0799
amaanq <amaanq12@gmail.com>
2022-09-25 06:11:35 -0400

    few changes

--- neovim/.config/nvim/lua/amaanq/plugins/lsp/nvim-cmp.lua
+++ neovim/.config/nvim/lua/amaanq/plugins/lsp/nvim-cmp.lua
@@ -38,7 +38,8 @@ cmp.setup({
         ['<C-e>'] = cmp.config.disable,
         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        -- ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
         ['<C-Space>'] = cmp.mapping.complete(),
     }),
     sources = cmp.config.sources({
--- neovim/.config/nvim/lua/amaanq/plugins/toggleterm.lua
+++ neovim/.config/nvim/lua/amaanq/plugins/toggleterm.lua
@@ -11,3 +11,12 @@ require("toggleterm").setup({
     close_on_exit = true,
     shell = vim.o.shell,
 });
+
+local Terminal  = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+
+function _lazygit_toggle()
+  lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
