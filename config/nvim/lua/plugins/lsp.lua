c8acdfd39f2cdfa3283ef482096708228723dd38
Amaan Qureshi <amaanq12@gmail.com>
2023-01-04 18:00:55 -0500

    readd java lsp

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -67,6 +67,7 @@ return {
 			golangci_lint_ls = {},
 			gopls = {},
 			html = {},
+			jdtls = {},
 			jsonls = {
 				on_new_config = function(new_config)
 					new_config.settings.json.schemas = new_config.settings.json.schemas or {}
