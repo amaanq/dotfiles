a1b21d8f45e7614132e1e1e194388cf93f121433
Amaan Q <amaanq12@gmail.com>
2023-01-04 17:45:51 -0500

    refactoring/keeping up-to-date

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -25,7 +25,6 @@ return {
 
 	-- auto-resize windows
 	{
-		enabled = true,
 		"anuvyklack/windows.nvim",
 		event = "WinNew",
 		dependencies = {
@@ -129,6 +128,44 @@ return {
 			math.randomseed(os.time())
 			local theme = ({ "stars", "snow", "xmas" })[math.random(1, 3)]
 			require("drop").setup({ theme = theme, max = 60, interval = 50 })
+		end,
+	},
+
+	-- animations
+	{
+		"echasnovski/mini.animate",
+		event = "VeryLazy",
+		config = function()
+			local mouse_scrolled = false
+			for _, scroll in ipairs({ "Up", "Down" }) do
+				local key = "<ScrollWheel" .. scroll .. ">"
+				vim.keymap.set("", key, function()
+					mouse_scrolled = true
+					return key
+				end, { expr = true })
+			end
+
+			local animate = require("mini.animate")
+			vim.go.winwidth = 20
+			vim.go.winminwidth = 5
+
+			animate.setup({
+				resize = {
+					timing = animate.gen_timing.linear({ duration = 100, unit = "total" }),
+				},
+				scroll = {
+					timing = animate.gen_timing.linear({ duration = 150, unit = "total" }),
+					subscroll = animate.gen_subscroll.equal({
+						predicate = function(total_scroll)
+							if mouse_scrolled then
+								mouse_scrolled = false
+								return false
+							end
+							return total_scroll > 1
+						end,
+					}),
+				},
+			})
 		end,
 	},
 
