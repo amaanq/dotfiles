df46516353610055b057f2e6f5287aa9496d99aa
Amaan Q <amaanq12@gmail.com>
2023-01-06 03:30:49 -0500

    feat: disable built-ins and add some nice-to-have globals

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,5 +1,24 @@
 vim.g.mapleader = " "
 
+vim.g.os = vim.loop.os_uname().sysname
+vim.g.open_command = vim.g.os == "Darwin" and "open" or "xdg-open"
+vim.g.dotfiles = vim.env.DOTFILES or vim.fn.expand("~/.dotfiles")
+vim.g.vim_dir = vim.g.dotfiles .. "/.config/nvim"
+
+----------------------------------------------------------------------------------------------------
+-- Default plugins
+----------------------------------------------------------------------------------------------------
+-- Stop loading built in plugins
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_tutor_mode_plugin = 1
+vim.g.loaded_2html_plugin = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.logipat = 1
+
+-- Ensure all autocommands are cleared
+vim.api.nvim_create_augroup("vimrc", {})
+
 if vim.fn.has("nvim-0.8") == 1 then
 	vim.opt.backup = true
 	vim.opt.cmdheight = 0
