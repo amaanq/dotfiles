be82d14703a45c9d22ae39dba9717b5ef3339df9
Amaan Qureshi <amaanq12@gmail.com>
2025-06-04 05:44:22 -0400

    feat(nix): darwin dock

--- modules/darwin/dock.nix
+++ modules/darwin/dock.nix
@@ -14,7 +14,14 @@
     expose-animation-duration = 0.1;
 
     persistent-apps = [
+      { app = "/Applications/Thorium.app"; }
       { app = "/Applications/Ghostty.app"; }
+      { app = "/Applications/Discord.app"; }
+      { app = "/Applications/Nix Apps/Spotify.app"; }
+      { app = "/Applications/Nix Apps/Element.app"; }
+      { app = "/Applications/Nix Apps/Telegram.app"; }
+      { app = "/Applications/Nix Apps/IINA.app"; }
+      { app = "/Applications/Nix Apps/qbittorrent.app"; }
     ];
   };
 }
