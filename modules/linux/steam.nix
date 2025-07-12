6ae51df30b22c3ace6b7685f8f8a55a18c3d3f25
Amaan Qureshi <amaanq12@gmail.com>
2025-07-12 15:52:34 -0400

    feat: add (updated) r2modman

--- modules/linux/steam.nix
+++ modules/linux/steam.nix
@@ -6,13 +6,39 @@
 }:
 let
   inherit (lib) attrValues merge mkIf;
+
+  # Override r2modman to version 3.2.1
+  r2modman-updated = pkgs.r2modman.overrideAttrs (oldAttrs: {
+    version = "3.2.1";
+    src = pkgs.fetchFromGitHub {
+      owner = "ebkr";
+      repo = "r2modmanPlus";
+      rev = "v3.2.1";
+      hash = "sha256-l1xrp+Gl26kiWqh5pIKp4QiETrzr5mTrUP10T0DhUCw=";
+    };
+    offlineCache = pkgs.fetchYarnDeps {
+      yarnLock = "${
+        pkgs.fetchFromGitHub {
+          owner = "ebkr";
+          repo = "r2modmanPlus";
+          rev = "v3.2.1";
+          hash = "sha256-l1xrp+Gl26kiWqh5pIKp4QiETrzr5mTrUP10T0DhUCw=";
+        }
+      }/yarn.lock";
+      hash = "sha256-HLVHxjyymi0diurVamETrfwYM2mkUrIOHhbYCrqGkeg=";
+    };
+  });
 in
 merge
 <| mkIf config.isDesktop {
+  unfree.allowedNames = [
+    "steam"
+    "steam-unwrapped"
+  ];
+
   # Steam uses 32-bit drivers for some unholy fucking reason.
   hardware.graphics.enable32Bit = true;
-
   environment.systemPackages = attrValues {
-    inherit (pkgs) steam;
+    inherit (pkgs) steam r2modman;
   };
 }
