14b5dc445c25204fb695f3c641f96d10fd570753
Amaan Qureshi <amaanq12@gmail.com>
2025-07-23 00:48:00 -0400

    feat: add man pages

--- modules/linux/documentation.nix
+++ modules/linux/documentation.nix
@@ -1,11 +1,17 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 let
   inherit (lib) disabled enabled;
 in
 {
   documentation = {
     doc = disabled;
+    dev = enabled;
     info = disabled;
     man = enabled;
   };
+
+  environment.systemPackages = [
+    pkgs.man-pages
+    pkgs.man-pages-posix
+  ];
 }
