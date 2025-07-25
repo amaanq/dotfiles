eec537a71156a4289fbd7fb5877fc04e90167658
Amaan Qureshi <amaanq12@gmail.com>
2025-07-25 16:25:21 -0400

    feat: always enable xdg

--- modules/common/home-manager.nix
+++ modules/common/home-manager.nix
@@ -1,6 +1,16 @@
+{ lib, ... }:
+let
+  inherit (lib) enabled;
+in
 {
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
+
+    sharedModules = [
+      {
+        xdg = enabled { };
+      }
+    ];
   };
 }
