abc113c2c776ffba4059ebe76d9d2737194683c4
Amaan Qureshi <amaanq12@gmail.com>
2025-08-03 23:33:38 -0400

    chore: remove r2modman patch

--- modules/linux/steam.nix
+++ modules/linux/steam.nix
@@ -5,33 +5,7 @@
   ...
 }:
 let
-  inherit (lib)
-    enabled
-    merge
-    mkIf
-    ;
-
-  # Override r2modman to version 3.2.1
-  r2modman-updated = pkgs.r2modman.overrideAttrs (oldAttrs: {
-    version = "3.2.1";
-    src = pkgs.fetchFromGitHub {
-      owner = "ebkr";
-      repo = "r2modmanPlus";
-      rev = "v3.2.1";
-      hash = "sha256-l1xrp+Gl26kiWqh5pIKp4QiETrzr5mTrUP10T0DhUCw=";
-    };
-    offlineCache = pkgs.fetchYarnDeps {
-      yarnLock = "${
-        pkgs.fetchFromGitHub {
-          owner = "ebkr";
-          repo = "r2modmanPlus";
-          rev = "v3.2.1";
-          hash = "sha256-l1xrp+Gl26kiWqh5pIKp4QiETrzr5mTrUP10T0DhUCw=";
-        }
-      }/yarn.lock";
-      hash = "sha256-HLVHxjyymi0diurVamETrfwYM2mkUrIOHhbYCrqGkeg=";
-    };
-  });
+  inherit (lib) enabled merge mkIf;
 in
 merge
 <| mkIf config.isDesktop {
@@ -45,8 +19,8 @@ merge
   # Steam uses 32-bit drivers for some unholy fucking reason.
   hardware.graphics.enable32Bit = true;
   environment.systemPackages = [
-    pkgs.steam
     pkgs.mangohud
-    r2modman-updated
+    pkgs.steam
+    pkgs.r2modman
   ];
 }
