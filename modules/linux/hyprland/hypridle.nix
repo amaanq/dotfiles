228e859b8052bed7397b23649640ce9ea4c39a03
Amaan Qureshi <amaanq12@gmail.com>
2025-08-05 21:46:30 -0400

    fix(hypridle): remove unnecessary settings

--- modules/linux/hyprland/hypridle.nix
+++ modules/linux/hyprland/hypridle.nix
@@ -14,8 +14,6 @@ merge
         settings = {
           general = {
             lock_cmd = "pidof hyprlock || hyprlock";
-            before_sleep_cmd = "loginctl lock-session";
-            after_sleep_cmd = "hyprctl dispatch dpms on";
           };
 
           listener = [
