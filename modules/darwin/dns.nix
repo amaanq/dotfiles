837ed44c19aebb601f7a9f038ca72dfce14971ee
Amaan Qureshi <amaanq12@gmail.com>
2025-08-09 06:33:14 -0400

    feat(darwin): dns

---
+++ modules/darwin/dns.nix
@@ -0,0 +1,14 @@
+{ config, lib, ... }:
+let
+  inherit (lib) head splitString;
+in
+{
+  # Yeah, no DNSSEC or DoT or anything.
+  # That's what you get for using Darwin I guess.
+  networking.dns = config.dns.servers |> map (splitString "#") |> map head;
+
+  networking.knownNetworkServices = [
+    "Thunderbolt Bridge"
+    "Wi-Fi"
+  ];
+}
