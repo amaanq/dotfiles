14562504059d28db185c38ab5c8ee4ba3ab86bba
Amaan Qureshi <amaanq12@gmail.com>
2025-08-16 17:38:51 -0400

    feat: add xdg mime apps

--- modules/common/home-manager.nix
+++ modules/common/home-manager.nix
@@ -9,7 +9,44 @@ in
 
     sharedModules = [
       {
-        xdg = enabled { };
+        xdg = enabled {
+          mimeApps = enabled {
+            defaultApplications = {
+              # Browser
+              "text/html" = "thorium-browser.desktop";
+              "x-scheme-handler/about" = "thorium-browser.desktop";
+              "x-scheme-handler/chrome" = "thorium-browser.desktop";
+              "x-scheme-handler/http" = "thorium-browser.desktop";
+              "x-scheme-handler/https" = "thorium-browser.desktop";
+              "x-scheme-handler/unknown" = "thorium-browser.desktop";
+              "application/x-extension-htm" = "thorium-browser.desktop";
+              "application/x-extension-html" = "thorium-browser.desktop";
+              "application/x-extension-shtml" = "thorium-browser.desktop";
+              "application/xhtml+xml" = "thorium-browser.desktop";
+              "application/x-extension-xhtml" = "thorium-browser.desktop";
+              "application/x-extension-xht" = "thorium-browser.desktop";
+
+              "x-scheme-handler/ror2mm" = "r2modman.desktop";
+              "x-scheme-handler/tg" = "org.telegram.desktop.desktop";
+              "x-scheme-handler/tonsite" = "org.telegram.desktop.desktop";
+              "x-scheme-handler/discord" = "vesktop.desktop";
+
+              "x-scheme-handler/mailto" = "userapp-Thunderbird-3NCNA3.desktop";
+              "message/rfc822" = "userapp-Thunderbird-3NCNA3.desktop";
+              "x-scheme-handler/mid" = "userapp-Thunderbird-3NCNA3.desktop";
+              "x-scheme-handler/news" = "userapp-Thunderbird-95BLA3.desktop";
+              "x-scheme-handler/snews" = "userapp-Thunderbird-95BLA3.desktop";
+              "x-scheme-handler/nntp" = "userapp-Thunderbird-95BLA3.desktop";
+              "x-scheme-handler/feed" = "userapp-Thunderbird-YEKOA3.desktop";
+              "application/rss+xml" = "userapp-Thunderbird-YEKOA3.desktop";
+              "application/x-extension-rss" = "userapp-Thunderbird-YEKOA3.desktop";
+              "x-scheme-handler/webcal" = "userapp-Thunderbird-YMTHA3.desktop";
+              "text/calendar" = "userapp-Thunderbird-YMTHA3.desktop";
+              "application/x-extension-ics" = "userapp-Thunderbird-YMTHA3.desktop";
+              "x-scheme-handler/webcals" = "userapp-Thunderbird-YMTHA3.desktop";
+            };
+          };
+        };
       }
     ];
   };
