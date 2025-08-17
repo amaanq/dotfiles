9d81371344a081c5b5f760318d9bc0d85ed2181e
Amaan Qureshi <amaanq12@gmail.com>
2025-08-16 22:09:44 -0400

    fix: only install android udev rules on linux

--- modules/common/android.nix
+++ modules/common/android.nix
@@ -8,22 +8,13 @@ let
   inherit (lib)
     merge
     mkIf
+    optionals
     ;
 in
 merge
 <| mkIf config.isDesktop {
   unfree.allowedNames = [
     "android-sdk-emulator"
-    "android-sdk-system-image-32-google_apis-arm64-v8a-system-image-32-google_apis-x86_64"
-    "android-sdk-system-image-32-google_apis_playstore-arm64-v8a-system-image-32-google_apis_playstore-x86_64"
-    "android-sdk-system-image-33-google_apis-arm64-v8a-system-image-33-google_apis-x86_64"
-    "android-sdk-system-image-33-google_apis_playstore-arm64-v8a-system-image-33-google_apis_playstore-x86_64"
-    "android-sdk-system-image-34-google_apis-arm64-v8a-system-image-34-google_apis-x86_64"
-    "android-sdk-system-image-34-google_apis_playstore-arm64-v8a-system-image-34-google_apis_playstore-x86_64"
-    "android-sdk-system-image-35-google_apis-arm64-v8a-system-image-35-google_apis-x86_64"
-    "android-sdk-system-image-35-google_apis_playstore-arm64-v8a-system-image-35-google_apis_playstore-x86_64"
-    "android-sdk-system-image-36-google_apis-arm64-v8a-system-image-36-google_apis-x86_64"
-    "android-sdk-system-image-36-google_apis_playstore-arm64-v8a-system-image-36-google_apis_playstore-x86_64"
     "android-sdk-ndk"
     "android-sdk-platform-tools"
     "android-sdk-tools"
@@ -31,9 +22,11 @@ merge
 
   environment.systemPackages = [
     pkgs.android-tools
-    pkgs.android-udev-rules
     pkgs.androidenv.androidPkgs.ndk-bundle
     pkgs.androidenv.androidPkgs.platform-tools
     pkgs.androidenv.androidPkgs.tools
+  ]
+  ++ optionals config.isLinux [
+    pkgs.android-udev-rules
   ];
 }
