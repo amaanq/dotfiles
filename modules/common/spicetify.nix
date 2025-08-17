f3d6c4becd6f8bedf6950596852ab7065ff20c73
Amaan Qureshi <amaanq12@gmail.com>
2025-08-16 21:49:48 -0400

    fix(spicetify): enable wayland only on linux

--- modules/common/spicetify.nix
+++ modules/common/spicetify.nix
@@ -6,16 +6,24 @@
   ...
 }:
 let
-  inherit (lib) enabled merge mkIf;
+  inherit (lib)
+    enabled
+    merge
+    mkIf
+    optionalAttrs
+    ;
 in
 merge
 <| mkIf config.isDesktop {
-  programs.spicetify = enabled {
-    wayland = true;
-    experimentalFeatures = true;
-    enabledExtensions = with inputs.spicetify.legacyPackages.${pkgs.stdenv.system}.extensions; [
-      shuffle
-      copyLyrics
-    ];
-  };
+  programs.spicetify =
+    enabled {
+      experimentalFeatures = true;
+      enabledExtensions = with inputs.spicetify.legacyPackages.${pkgs.stdenv.system}.extensions; [
+        shuffle
+        copyLyrics
+      ];
+    }
+    // optionalAttrs config.isLinux {
+      wayland = true;
+    };
 }
