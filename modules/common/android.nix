73df86e57cd6519cd3bb176cc21135ea2bd89b88
Amaan Qureshi <amaanq12@gmail.com>
2025-08-25 23:55:05 -0400

    android: update

--- modules/common/android.nix
+++ modules/common/android.nix
@@ -14,17 +14,16 @@ in
 merge
 <| mkIf config.isDesktop {
   unfree.allowedNames = [
-    "android-sdk-emulator"
     "android-sdk-ndk"
     "android-sdk-platform-tools"
-    "android-sdk-tools"
+    "platform-tools"
+    "ndk"
   ];
 
   environment.systemPackages = [
     pkgs.android-tools
     pkgs.androidenv.androidPkgs.ndk-bundle
     pkgs.androidenv.androidPkgs.platform-tools
-    pkgs.androidenv.androidPkgs.tools
   ]
   ++ optionals config.isLinux [
     pkgs.android-udev-rules
