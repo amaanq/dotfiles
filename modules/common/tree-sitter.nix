40e6ef438c7a5301219c10d17f4a2dd0547022cc
Amaan Qureshi <contact@amaanq.com>
2025-08-29 18:56:21 -0400

    tree-sitter: patch for 0.25.8

--- modules/common/tree-sitter.nix
+++ modules/common/tree-sitter.nix
@@ -7,6 +7,21 @@
   };
 
   environment.systemPackages = [
-    pkgs.tree-sitter
+    (pkgs.tree-sitter.overrideAttrs (old: rec {
+      version = "0.25.8";
+      src = pkgs.fetchFromGitHub {
+        owner = "tree-sitter";
+        repo = "tree-sitter";
+        tag = "v${version}";
+        hash = "sha256-q465DMTiFHoOZy6cMvrSywwO1qJVXPmQ0OVIPmwib6c=";
+        fetchSubmodules = true;
+      };
+
+      cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
+        inherit src;
+        hash = "sha256-5xtsNE94J5Hg8rGkyzx8P6c8vl1x17zgpSulcGNVKmI=";
+      };
+    }))
+    pkgs.python313Packages.pytest
   ];
 }
