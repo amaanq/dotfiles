444181d6a0a779a38efa4a3d60710ecce9737476
Amaan Qureshi <contact@amaanq.com>
2025-09-11 16:34:18 -0400

    tree-sitter: enable web-ui

--- modules/common/tree-sitter.nix
+++ modules/common/tree-sitter.nix
@@ -5,23 +5,26 @@
     tsa = "tree-sitter-alpha";
     tss = "tree-sitter-stable";
   };
-
   environment.systemPackages = [
-    (pkgs.tree-sitter.overrideAttrs (old: rec {
-      version = "0.25.9";
-      src = pkgs.fetchFromGitHub {
-        owner = "tree-sitter";
-        repo = "tree-sitter";
-        tag = "v${version}";
-        hash = "sha256-i7sptOJuLPSl0v8qYF54zfvVKOUtekcFedqapxehzWI=";
-        fetchSubmodules = true;
-      };
-
-      cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
-        inherit src;
-        hash = "sha256-0Do1UxIbfIfJ61dTiJt0ZGDrhOtGV0l9bafyoqcbqgU=";
-      };
-    }))
+    (
+      (pkgs.tree-sitter.override {
+        webUISupport = true;
+      }).overrideAttrs
+      (old: rec {
+        version = "0.25.9";
+        src = pkgs.fetchFromGitHub {
+          owner = "tree-sitter";
+          repo = "tree-sitter";
+          tag = "v${version}";
+          hash = "sha256-i7sptOJuLPSl0v8qYF54zfvVKOUtekcFedqapxehzWI=";
+          fetchSubmodules = true;
+        };
+        cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
+          inherit src;
+          hash = "sha256-0Do1UxIbfIfJ61dTiJt0ZGDrhOtGV0l9bafyoqcbqgU=";
+        };
+      })
+    )
     pkgs.python313Packages.pytest
   ];
 }
