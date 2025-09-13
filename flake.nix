faa2a31926fae6a0096ab2ca7508d3a7c406e81e
Amaan Qureshi <contact@amaanq.com>
2025-09-13 18:06:01 -0400

    claude-desktop: use fork

--- flake.nix
+++ flake.nix
@@ -125,7 +125,7 @@
     };
 
     claude-desktop = {
-      url = "github:k3d3/claude-desktop-linux-flake";
+      url = "github:amaanq/claude-desktop-linux-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
