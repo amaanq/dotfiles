b474009676a3d899c151669a4fbd1e6e107de15f
Amaan Qureshi <git@amaanq.com>
2025-09-22 17:39:19 -0400

    virtualization: add virtd

--- modules/linux/virtualization.nix
+++ modules/linux/virtualization.nix
@@ -1,10 +1,28 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   inherit (lib) enabled merge mkIf;
 in
 merge
 <| mkIf config.isDesktop {
+  services.spice-vdagentd = enabled;
+
   virtualisation = {
     waydroid = enabled;
+
+    libvirtd = enabled {
+      qemu = {
+        swtpm = enabled;
+        ovmf = enabled {
+          packages = [ pkgs.OVMFFull.fd ];
+        };
+      };
+    };
+
+    spiceUSBRedirection.enable = true;
   };
 }
