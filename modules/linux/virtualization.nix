e23afd78cae40932e6dff7459f5b8b8ff5845369
Amaan Qureshi <git@amaanq.com>
2025-09-23 17:07:24 -0400

    virtualization: update

--- modules/linux/virtualization.nix
+++ modules/linux/virtualization.nix
@@ -9,20 +9,63 @@ let
 in
 merge
 <| mkIf config.isDesktop {
+  boot.kernelModules = [
+    "vfio-pci"
+  ];
+  boot.kernelParams = [
+    "amd_iommu=on"
+    "iommu=pt"
+    "vfio-pci.ids="
+  ];
+
+  environment = {
+    systemPackages = [
+      pkgs.virt-viewer
+      pkgs.virtio-win
+      pkgs.looking-glass-client
+      pkgs.win-spice
+    ];
+  };
+
+  home-manager.sharedModules = [
+    {
+      dconf.settings = {
+        "org/virt-manager/virt-manager/connections" = {
+          autoconnect = [ "qemu:///system" ];
+          uris = [ "qemu:///system" ];
+        };
+      };
+    }
+  ];
+
+  programs.dconf = enabled;
+  programs.virt-manager = enabled;
+
   services.spice-vdagentd = enabled;
+
+  users.users.amaanq = {
+    extraGroups = [
+      "libvirtd"
+      "kvm"
+    ];
+  };
 
   virtualisation = {
     waydroid = enabled;
 
     libvirtd = enabled {
       qemu = {
+        package = pkgs.qemu;
         swtpm = enabled;
         ovmf = enabled {
           packages = [ pkgs.OVMFFull.fd ];
         };
+        vhostUserPackages = [ pkgs.virtiofsd ];
       };
+      onBoot = "ignore";
+      onShutdown = "shutdown";
     };
 
-    spiceUSBRedirection.enable = true;
+    spiceUSBRedirection = enabled;
   };
 }
