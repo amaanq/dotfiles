115eb6977819328e7f111d094c43149af675ac8a
Amaan Qureshi <git@amaanq.com>
2025-09-28 19:21:10 -0400

    feat: remove hyprland

--- modules/common/stylix.nix
+++ modules/common/stylix.nix
@@ -31,14 +31,11 @@ in
       stylix = enabled {
         autoEnable = true;
         cursor = {
-          name = "rose-pine-hyprcursor";
-          package = pkgs.rose-pine-hyprcursor;
           size = 24;
         };
         targets = {
           gtk = disabled;
           helix = disabled;
-          hyprlock = disabled;
           kitty = disabled;
           ghostty = disabled;
           zen-browser.profileNames = [ "default" ];
