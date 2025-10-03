a8d78ca1d9c82893742d9051b33b9669b6bbc313
Amaan Qureshi <git@amaanq.com>
2025-10-03 18:33:57 -0400

    lib: remove colors

--- lib/default.nix
+++ lib/default.nix
@@ -1,9 +1,8 @@
 inputs: self: super:
 let
-  colors = import ./colors.nix inputs self super;
   filesystem = import ./filesystem.nix inputs self super;
   option = import ./option.nix inputs self super;
   system = import ./system.nix inputs self super;
   values = import ./values.nix inputs self super;
 in
-colors // filesystem // option // system // values
+filesystem // option // system // values
