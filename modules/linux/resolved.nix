a6a7d88b3cec0d1903acea5837f140b1975b35e7
Amaan Qureshi <git@amaanq.com>
2025-10-07 02:11:27 -0400

    dns: ignore DHCP/RA DNS servers (fuck Verizon)

--- modules/linux/resolved.nix
+++ modules/linux/resolved.nix
@@ -11,4 +11,11 @@ in
 
     fallbackDns = config.dns.serversFallback;
   };
+
+  systemd.network.networks."99-ethernet-default-dhcp".dhcpV4Config.UseDNS = false;
+  systemd.network.networks."99-ethernet-default-dhcp".dhcpV6Config.UseDNS = false;
+  systemd.network.networks."99-ethernet-default-dhcp".ipv6AcceptRAConfig.UseDNS = false;
+  systemd.network.networks."99-wireless-client-dhcp".dhcpV4Config.UseDNS = false;
+  systemd.network.networks."99-wireless-client-dhcp".dhcpV6Config.UseDNS = false;
+  systemd.network.networks."99-wireless-client-dhcp".ipv6AcceptRAConfig.UseDNS = false;
 }
