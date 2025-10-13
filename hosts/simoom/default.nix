4fa0fe83f2d48611699dad23d5afd74086be3767
Amaan Qureshi <git@amaanq.com>
2025-10-13 00:38:46 -0400

    simoom: init

---
+++ hosts/simoom/default.nix
@@ -0,0 +1,37 @@
+lib:
+lib.darwinSystem' (
+  { lib, ... }:
+  let
+    inherit (lib) collectNix remove;
+  in
+  {
+    imports = collectNix ./. |> remove ./default.nix;
+
+    type = "desktop";
+
+    networking.hostName = "simoom";
+    nixpkgs.config.allowUnfree = true;
+    nix.enable = false;
+
+    users.users.amaanq = {
+      name = "amaanq";
+      home = "/Users/amaanq";
+    };
+
+    home-manager.users = {
+      amaanq = { };
+    };
+
+    system = {
+      primaryUser = "amaanq";
+      stateVersion = 5;
+    };
+    home-manager.sharedModules = [
+      {
+        home.stateVersion = "25.05";
+      }
+    ];
+
+    time.timeZone = "America/New_York";
+  }
+)
