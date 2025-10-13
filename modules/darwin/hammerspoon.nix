952dc3a4b91226f518c34762d8ba96c79a0ff853
Amaan Qureshi <git@amaanq.com>
2025-10-13 05:33:12 -0400

    hammerspoon: fix activation

--- modules/darwin/hammerspoon.nix
+++ modules/darwin/hammerspoon.nix
@@ -1,12 +1,13 @@
 {
-  system.defaults.CustomSystemPreferences."org.hammerspoon.Hammerspoon".MJConfigFile =
-    "~/.config/hammerspoon/init.lua";
-
   homebrew.casks = [ "hammerspoon" ];
 
   home-manager.sharedModules = [
     {
       xdg.configFile."hammerspoon/init.lua".text = "";
+
+      home.activation.setHammerspoonConfig = ''
+        $DRY_RUN_CMD /usr/bin/defaults write org.hammerspoon.Hammerspoon MJConfigFile ~/.config/hammerspoon/init.lua
+      '';
     }
   ];
 }
