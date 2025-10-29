1027810dd6946db1722e80002dcc9fa3d24b1d95
Amaan Qureshi <git@amaanq.com>
2025-10-29 01:27:47 -0400

    dock: update apps

--- modules/darwin/dock.nix
+++ modules/darwin/dock.nix
@@ -16,12 +16,12 @@
     persistent-apps = [
       { app = "/Applications/Ghostty.app"; }
       { app = "/Applications/Nix Apps/Thorium.app"; }
-      { app = "/Applications/Nix Apps/Discord.app"; }
+      { app = "/Applications/Orion.app"; }
+      { app = "~/Applications/Home Manager Apps/Discord.app"; }
       { app = "/Applications/Nix Apps/Spotify.app"; }
-      { app = "/Applications/Nix Apps/Element.app"; }
-      { app = "/Applications/Nix Apps/Telegram.app"; }
       { app = "/Applications/Nix Apps/IINA.app"; }
       { app = "/Applications/Nix Apps/qbittorrent.app"; }
+      { app = "/Applications/OrbStack.app"; }
     ];
   };
 
