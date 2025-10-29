777b7183d974bdd9499d2aea81b507dddf2163fd
Amaan Qureshi <git@amaanq.com>
2025-10-29 04:45:02 -0400

    desktop: don't pull in tuigreet and niri on non-desktops

--- modules/linux/desktop.nix
+++ modules/linux/desktop.nix
@@ -6,20 +6,23 @@
 }:
 let
   inherit (lib) enabled merge mkIf;
-  tuigreet = "${pkgs.tuigreet}/bin/tuigreet";
-  niri-session = "${pkgs.niri}/share/wayland-sessions";
 in
 merge
 <| mkIf config.isDesktop {
   # Display manager
-  services.greetd = enabled {
-    settings = {
-      default_session = {
-        command = "${tuigreet} --time --remember --remember-session --sessions ${niri-session}";
-        user = "greeter";
+  services.greetd =
+    let
+      tuigreet = "${pkgs.tuigreet}/bin/tuigreet";
+      niri-session = "${pkgs.niri}/share/wayland-sessions";
+    in
+    enabled {
+      settings = {
+        default_session = {
+          command = "${tuigreet} --time --remember --remember-session --sessions ${niri-session}";
+          user = "greeter";
+        };
       };
     };
-  };
 
   systemd.services.greetd.serviceConfig = {
     Type = "idle";
