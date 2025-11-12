d675f12369c98d255b341fa94156bf145c79a7c2
Amaan Qureshi <git@amaanq.com>
2025-11-12 12:31:46 -0500

    nm: remove tailscale hack, disable dns in favor of hickory-dns

--- modules/linux/network-manager.nix
+++ modules/linux/network-manager.nix
@@ -1,9 +1,4 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+{ config, lib, ... }:
 let
   inherit (lib)
     attrNames
@@ -18,23 +13,7 @@ in
 merge
 <| mkIf config.isDesktop {
   networking.networkmanager = enabled {
-    # lmfao https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/issues/1603
-    # we need to clear interface DNS after NM configures it
-    # so that DHCP/RA DNS isn't used by systemd-resolved
-    dispatcherScripts = [
-      {
-        source = pkgs.writeText "clear-interface-dns" ''
-          #!/bin/sh
-          # Skip Tailscale (ts0)
-          if [ "$DEVICE_IFACE" != "ts0" ]; then
-            if [ "$2" = "up" ] || [ "$2" = "dhcp4-change" ] || [ "$2" = "dhcp6-change" ]; then
-              ${pkgs.systemd}/bin/resolvectl dns "$DEVICE_IFACE" "" || true
-            fi
-          fi
-        '';
-        type = "basic";
-      }
-    ];
+    dns = "none";
   };
 
   users.extraGroups.networkmanager.members =
