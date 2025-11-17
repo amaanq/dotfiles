c0ebdf39a94a20fbe3fea7b08619ecbb1066c4fc
Amaan Qureshi <git@amaanq.com>
2025-11-17 02:01:15 -0500

    android: add avbroot and repo

--- modules/common/android.nix
+++ modules/common/android.nix
@@ -8,8 +8,18 @@ let
   inherit (lib)
     merge
     mkIf
-    optionals
     ;
+
+  avbroot = pkgs.avbroot.overrideAttrs (oldAttrs: rec {
+    version = "3.23.2";
+    src = pkgs.fetchFromGitHub {
+      owner = "chenxiaolong";
+      repo = "avbroot";
+      rev = "v${version}";
+      hash = "sha256-rS3V+D7dBt/px0UNC8ZZyQ4FzNsjTykMSeXbjFF6iis=";
+    };
+    cargoHash = "sha256-v1oR5z7g7jjJgPiE56wYA3s4bF41QV6JRs7iMumfKnI=";
+  });
 in
 merge
 <| mkIf config.isDesktop {
@@ -24,5 +34,7 @@ merge
     pkgs.android-tools
     pkgs.androidenv.androidPkgs.ndk-bundle
     pkgs.androidenv.androidPkgs.platform-tools
+    pkgs.git-repo
+    avbroot
   ];
 }
