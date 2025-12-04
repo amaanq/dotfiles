93a3f3150e733c60af100acf09092ce4075cdca0
Amaan Qureshi <git@amaanq.com>
2025-12-04 08:30:55 -0500

    tree-sitter: remove patch

--- modules/common/tree-sitter.nix
+++ modules/common/tree-sitter.nix
@@ -6,31 +6,7 @@
     tss = "tree-sitter-stable";
   };
   environment.systemPackages = [
-    (
-      (pkgs.tree-sitter.override {
-        webUISupport = true;
-      }).overrideAttrs
-      (
-        old:
-        let
-          version = "0.25.9";
-          src = pkgs.fetchFromGitHub {
-            owner = "tree-sitter";
-            repo = "tree-sitter";
-            tag = "v${version}";
-            hash = "sha256-i7sptOJuLPSl0v8qYF54zfvVKOUtekcFedqapxehzWI=";
-            fetchSubmodules = true;
-          };
-        in
-        {
-          inherit version src;
-          cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
-            inherit src;
-            hash = "sha256-0Do1UxIbfIfJ61dTiJt0ZGDrhOtGV0l9bafyoqcbqgU=";
-          };
-        }
-      )
-    )
+    pkgs.tree-sitter
     pkgs.python313Packages.pytest
   ];
 }
