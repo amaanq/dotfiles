5ed8f0887d27daa98292cb260b1e35a7a3d1715b
Amaan Qureshi <git@amaanq.com>
2025-12-04 03:11:12 -0500

    android: add sdk coal

--- modules/common/android.nix
+++ modules/common/android.nix
@@ -20,22 +20,57 @@ let
     };
     cargoHash = "sha256-v1oR5z7g7jjJgPiE56wYA3s4bF41QV6JRs7iMumfKnI=";
   });
+
+  androidComposition = pkgs.androidenv.composeAndroidPackages {
+    buildToolsVersions = [ "35.0.0" ];
+    includeNDK = true;
+    includeSources = false;
+    includeSystemImages = false;
+    includeEmulator = false;
+    extraLicenses = [
+      "android-sdk-license"
+    ];
+  };
 in
 merge
 <| mkIf config.isDesktop {
+  nixpkgs.config.android_sdk.accept_license = true;
+
   unfree.allowedNames = [
+    "android-studio-stable"
     "android-sdk-ndk"
+    "android-sdk-build-tools"
+    "android-sdk-cmdline-tools"
     "android-sdk-platform-tools"
-    "platform-tools"
+    "android-sdk-platforms"
+    "android-sdk-tools"
+    "build-tools"
+    "cmake"
+    "cmdline-tools"
     "ndk"
+    "platform-tools"
+    "platforms"
+    "tools"
   ];
 
   environment.systemPackages = [
+    pkgs.android-studio
     pkgs.android-tools
-    pkgs.androidenv.androidPkgs.ndk-bundle
-    pkgs.androidenv.androidPkgs.platform-tools
+    androidComposition.androidsdk
     pkgs.git-repo
+    pkgs.jadx
     pkgs.scrcpy
     avbroot
+  ];
+
+  environment.sessionVariables.ANDROID_SDK_ROOT = "${androidComposition.androidsdk}/libexec/android-sdk";
+
+  home-manager.sharedModules = [
+    (
+      { config, ... }:
+      {
+        home.sessionVariables.ANDROID_USER_HOME = "${config.xdg.dataHome}/android";
+      }
+    )
   ];
 }
