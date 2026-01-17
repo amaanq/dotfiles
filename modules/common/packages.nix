fc35096979d97a393a5d4962860fa9d26ace4dc0
Amaan Qureshi <git@amaanq.com>
2026-01-16 20:58:36 -0500

    claude-code: bump

--- modules/common/packages.nix
+++ modules/common/packages.nix
@@ -34,10 +34,10 @@ in
   environment.systemPackages = [
     pkgs.asciinema
     (pkgs.claude-code.overrideAttrs (old: {
-      version = "2.1.7";
+      version = "2.1.9";
       src = pkgs.fetchzip {
-        url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-2.1.7.tgz";
-        hash = "sha256-s/XPemwJYPUNFBgWo00VQ6W6eFIy44y9lFoRN0Duk9I=";
+        url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-2.1.9.tgz";
+        hash = "sha256-TU+54QVtcFaUErv8YB0NxmgP+0eUqa2JEjAVRHKPICs=";
       };
     }))
     pkgs.codex
