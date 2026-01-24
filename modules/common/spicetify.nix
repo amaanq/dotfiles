07d5b7c9e91214cabf6b0d93cf2199ab49d05463
Amaan Qureshi <git@amaanq.com>
2026-01-24 01:31:47 -0500

    spicetify: unredeem stylix

--- modules/common/spicetify.nix
+++ modules/common/spicetify.nix
@@ -9,10 +9,41 @@ let
   inherit (lib)
     enabled
     merge
+    mkForce
     mkIf
     optionals
     optionalAttrs
+    theme
     ;
+
+  colors = theme;
+
+  spicetifyTheme = pkgs.writeTextFile {
+    name = "color.ini";
+    destination = "/color.ini";
+    text = ''
+      [base]
+      text               = ${colors.text}
+      subtext            = ${colors.text}
+      main               = ${colors.base}
+      main-elevated      = ${colors.overlay}
+      highlight          = ${colors.overlay}
+      highlight-elevated = ${colors.muted}
+      sidebar            = ${colors.surface}
+      player             = ${colors.subtle}
+      card               = ${colors.muted}
+      shadow             = ${colors.base}
+      selected-row       = ${colors.subtle}
+      button             = ${colors.subtle}
+      button-active      = ${colors.subtle}
+      button-disabled    = ${colors.muted}
+      tab-active         = ${colors.overlay}
+      notification       = ${colors.overlay}
+      notification-error = ${colors.love}
+      equalizer          = ${colors.pine}
+      misc               = ${colors.overlay}
+    '';
+  };
 in
 merge
 <| mkIf config.isDesktop {
@@ -22,6 +53,12 @@ merge
       enabledExtensions = with spicetify.legacyPackages.${config.hostSystem}.extensions; [
         copyLyrics
       ];
+      theme = mkForce {
+        name = "rose-pine";
+        src = spicetifyTheme;
+        sidebarConfig = false;
+      };
+      colorScheme = mkForce "base";
     }
     // optionalAttrs config.isLinux {
       wayland = true;
