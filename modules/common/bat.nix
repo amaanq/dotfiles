86fe4df5e64f2cf77ec7adc0b255d48eeee78324
Amaan Qureshi <git@amaanq.com>
2026-01-24 00:08:15 -0500

    bat: unredeem hm

--- modules/common/bat.nix
+++ modules/common/bat.nix
@@ -1,22 +1,14 @@
-{ lib, ... }:
-let
-  inherit (lib) enabled;
-in
+{ pkgs, ... }:
 {
+  environment.systemPackages = [ pkgs.bat ];
+
   environment.variables = {
     MANPAGER = "bat --plain";
     PAGER = "bat --plain";
   };
+
   environment.shellAliases = {
     cat = "bat";
     less = "bat --plain";
   };
-
-  home-manager.sharedModules = [
-    {
-      programs.bat = enabled {
-        config.pager = "less -FR";
-      };
-    }
-  ];
 }
