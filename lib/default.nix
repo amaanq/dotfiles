fb268ca7729954d3c25c51b5f4b54987bbe08ed0
Amaan Qureshi <git@amaanq.com>
2026-01-24 01:36:37 -0500

    theme: add

--- lib/default.nix
+++ lib/default.nix
@@ -4,5 +4,6 @@ let
   option = import ./option.nix inputs self super;
   system = import ./system.nix inputs self super;
   values = import ./values.nix inputs self super;
+  theme = import ./theme.nix;
 in
-filesystem // option // system // values
+filesystem // option // system // values // { inherit theme; }
---
+++ lib/theme.nix
@@ -0,0 +1,74 @@
+# Rose Pine color scheme
+# https://rosepinetheme.com/palette
+{
+  # Base16 mapping for rose-pine
+  base00 = "191724"; # base
+  base01 = "1f1d2e"; # surface
+  base02 = "26233a"; # overlay
+  base03 = "6e6a86"; # muted
+  base04 = "908caa"; # subtle
+  base05 = "e0def4"; # text
+  base06 = "e0def4"; # text
+  base07 = "524f67"; # highlight med
+  base08 = "eb6f92"; # love (red)
+  base09 = "f6c177"; # gold (orange)
+  base0A = "ebbcba"; # rose (yellow)
+  base0B = "31748f"; # pine (green)
+  base0C = "9ccfd8"; # foam (cyan)
+  base0D = "c4a7e7"; # iris (blue)
+  base0E = "c4a7e7"; # iris (magenta)
+  base0F = "eb6f92"; # love (brown)
+
+  # Named colors for convenience
+  base = "191724";
+  surface = "1f1d2e";
+  overlay = "26233a";
+  muted = "6e6a86";
+  subtle = "908caa";
+  text = "e0def4";
+  love = "eb6f92";
+  gold = "f6c177";
+  rose = "ebbcba";
+  pine = "31748f";
+  foam = "9ccfd8";
+  iris = "c4a7e7";
+  highlightLow = "21202e";
+  highlightMed = "403d52";
+  highlightHigh = "524f67";
+
+  # With # prefix
+  withHashtag = {
+    base00 = "#191724";
+    base01 = "#1f1d2e";
+    base02 = "#26233a";
+    base03 = "#6e6a86";
+    base04 = "#908caa";
+    base05 = "#e0def4";
+    base06 = "#e0def4";
+    base07 = "#524f67";
+    base08 = "#eb6f92";
+    base09 = "#f6c177";
+    base0A = "#ebbcba";
+    base0B = "#31748f";
+    base0C = "#9ccfd8";
+    base0D = "#c4a7e7";
+    base0E = "#c4a7e7";
+    base0F = "#eb6f92";
+
+    base = "#191724";
+    surface = "#1f1d2e";
+    overlay = "#26233a";
+    muted = "#6e6a86";
+    subtle = "#908caa";
+    text = "#e0def4";
+    love = "#eb6f92";
+    gold = "#f6c177";
+    rose = "#ebbcba";
+    pine = "#31748f";
+    foam = "#9ccfd8";
+    iris = "#c4a7e7";
+    highlightLow = "#21202e";
+    highlightMed = "#403d52";
+    highlightHigh = "#524f67";
+  };
+}
