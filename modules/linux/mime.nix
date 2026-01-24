589fe2a2ab81426e33fb1aca589f85735ef94106
Amaan Qureshi <git@amaanq.com>
2026-01-24 18:48:22 -0500

    mime: unredeem hm

--- modules/linux/mime.nix
+++ modules/linux/mime.nix
@@ -1,47 +1,34 @@
-{ lib, ... }:
-let
-  inherit (lib) enabled;
-in
-{
-  home-manager.sharedModules = [
-    {
-      xdg.mimeApps = enabled {
-        defaultApplications = {
-          # Browser
-          "text/html" = "helium.desktop";
-          "x-scheme-handler/about" = "helium.desktop";
-          "x-scheme-handler/chrome" = "helium.desktop";
-          "x-scheme-handler/http" = "helium.desktop";
-          "x-scheme-handler/https" = "helium.desktop";
-          "x-scheme-handler/unknown" = "helium.desktop";
-          "application/x-extension-htm" = "helium.desktop";
-          "application/x-extension-html" = "helium.desktop";
-          "application/x-extension-shtml" = "helium.desktop";
-          "application/xhtml+xml" = "helium.desktop";
-          "application/x-extension-xhtml" = "helium.desktop";
-          "application/x-extension-xht" = "helium.desktop";
+_: {
+  xdg.mime.defaultApplications = {
+    # Browser
+    "text/html" = "helium.desktop";
+    "x-scheme-handler/about" = "helium.desktop";
+    "x-scheme-handler/chrome" = "helium.desktop";
+    "x-scheme-handler/http" = "helium.desktop";
+    "x-scheme-handler/https" = "helium.desktop";
+    "x-scheme-handler/unknown" = "helium.desktop";
+    "application/x-extension-htm" = "helium.desktop";
+    "application/x-extension-html" = "helium.desktop";
+    "application/x-extension-shtml" = "helium.desktop";
+    "application/xhtml+xml" = "helium.desktop";
+    "application/x-extension-xhtml" = "helium.desktop";
+    "application/x-extension-xht" = "helium.desktop";
 
-          "x-scheme-handler/ror2mm" = "r2modman.desktop";
-          "x-scheme-handler/tg" = "web-app-Telegram.desktop";
-          "x-scheme-handler/tonsite" = "web-app-Telegram.desktop";
-          "x-scheme-handler/matrix" = "web-app-Element.desktop";
-          "x-scheme-handler/discord" = "vesktop.desktop";
+    "x-scheme-handler/ror2mm" = "r2modman.desktop";
+    "x-scheme-handler/discord" = "vesktop.desktop";
 
-          "x-scheme-handler/mailto" = "userapp-Thunderbird-3NCNA3.desktop";
-          "message/rfc822" = "userapp-Thunderbird-3NCNA3.desktop";
-          "x-scheme-handler/mid" = "userapp-Thunderbird-3NCNA3.desktop";
-          "x-scheme-handler/news" = "userapp-Thunderbird-95BLA3.desktop";
-          "x-scheme-handler/snews" = "userapp-Thunderbird-95BLA3.desktop";
-          "x-scheme-handler/nntp" = "userapp-Thunderbird-95BLA3.desktop";
-          "x-scheme-handler/feed" = "userapp-Thunderbird-YEKOA3.desktop";
-          "application/rss+xml" = "userapp-Thunderbird-YEKOA3.desktop";
-          "application/x-extension-rss" = "userapp-Thunderbird-YEKOA3.desktop";
-          "x-scheme-handler/webcal" = "userapp-Thunderbird-YMTHA3.desktop";
-          "text/calendar" = "userapp-Thunderbird-YMTHA3.desktop";
-          "application/x-extension-ics" = "userapp-Thunderbird-YMTHA3.desktop";
-          "x-scheme-handler/webcals" = "userapp-Thunderbird-YMTHA3.desktop";
-        };
-      };
-    }
-  ];
+    "x-scheme-handler/mailto" = "userapp-Thunderbird-3NCNA3.desktop";
+    "message/rfc822" = "userapp-Thunderbird-3NCNA3.desktop";
+    "x-scheme-handler/mid" = "userapp-Thunderbird-3NCNA3.desktop";
+    "x-scheme-handler/news" = "userapp-Thunderbird-95BLA3.desktop";
+    "x-scheme-handler/snews" = "userapp-Thunderbird-95BLA3.desktop";
+    "x-scheme-handler/nntp" = "userapp-Thunderbird-95BLA3.desktop";
+    "x-scheme-handler/feed" = "userapp-Thunderbird-YEKOA3.desktop";
+    "application/rss+xml" = "userapp-Thunderbird-YEKOA3.desktop";
+    "application/x-extension-rss" = "userapp-Thunderbird-YEKOA3.desktop";
+    "x-scheme-handler/webcal" = "userapp-Thunderbird-YMTHA3.desktop";
+    "text/calendar" = "userapp-Thunderbird-YMTHA3.desktop";
+    "application/x-extension-ics" = "userapp-Thunderbird-YMTHA3.desktop";
+    "x-scheme-handler/webcals" = "userapp-Thunderbird-YMTHA3.desktop";
+  };
 }
