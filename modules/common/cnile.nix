0e3829da4631dc59a14948d582efb51c4e6efc18
Amaan Qureshi <git@amaanq.com>
2026-01-24 18:57:41 -0500

    cnile: unredeem hm

--- modules/common/cnile.nix
+++ modules/common/cnile.nix
@@ -13,33 +13,30 @@
     pkgs.stdenv.cc.cc.lib
   ];
 
-  # "Root" clang-format and clang-tidy configuration files
-  home-manager.sharedModules = [
-    {
-      home.file.".clang-format".text = ''
-        AlignArrayOfStructures: Left
-        BasedOnStyle: LLVM
-        IndentCaseLabels: true
-        IndentGotoLabels: true
-        IndentPPDirectives: None
-        IndentWidth: 4
-        InsertNewlineAtEOF: true
-        KeepEmptyLinesAtTheStartOfBlocks: false
-        SeparateDefinitionBlocks: Always
-        SortIncludes: CaseInsensitive
-        SortUsingDeclarations: true
-        SpaceAfterCStyleCast: false
-        SpaceAfterLogicalNot: false
-        SpaceBeforeCaseColon: false
-        BinPackParameters: true
-        BinPackArguments: true
-        ColumnLimit: 120
-      '';
+  # Root clang-format and clang-tidy configs
+  # TODO: Upstream support for arbitrary root/fallback configs
+  environment.etc."clang-format".text = ''
+    AlignArrayOfStructures: Left
+    BasedOnStyle: LLVM
+    IndentCaseLabels: true
+    IndentGotoLabels: true
+    IndentPPDirectives: None
+    IndentWidth: 4
+    InsertNewlineAtEOF: true
+    KeepEmptyLinesAtTheStartOfBlocks: false
+    SeparateDefinitionBlocks: Always
+    SortIncludes: CaseInsensitive
+    SortUsingDeclarations: true
+    SpaceAfterCStyleCast: false
+    SpaceAfterLogicalNot: false
+    SpaceBeforeCaseColon: false
+    BinPackParameters: true
+    BinPackArguments: true
+    ColumnLimit: 120
+  '';
 
-      home.file.".clang-tidy".text = ''
-        Checks: "*,clang-analyzer-optin-performance.Padding,-modernize-use-trailing-return-type,-altera-*,-google-readability-todo,-cppcoreguidelines-avoid-magic-numbers,-llvmlibc-*,-readability-magic-numbers,-hicpp-signed-bitwise,-readability-function-cognitive-complexity,-google-objc-*,-hicpp-avoid-c-arrays,-hicpp-no-array-decay,-fuchsia-*,-cppcoreguidelines-owning-memory,-google-build-using-namespace,-modernize-avoid-c-arrays,-cppcoreguidelines-pro-bounds-array-to-pointer-decay,-cppcoreguidelines-avoid-c-arrays,-cppcoreguidelines-avoid-non-const-global-variables,-performance-no-int-to-ptr,-cert-dc*,-bugprone-reserved-identifier,-hicpp-no-assembler,-readability-isolate-declaration,-cppcoreguidelines-init-variables,-readability-identifier-length,-bugprone-easily-swappable-parameters,-cert-err33-c,-misc-non-private-member-variables-in-classes"
-        HeaderFilterRegex: ""
-      '';
-    }
-  ];
+  environment.etc."clang-tidy".text = ''
+    Checks: "*,clang-analyzer-optin-performance.Padding,-modernize-use-trailing-return-type,-altera-*,-google-readability-todo,-cppcoreguidelines-avoid-magic-numbers,-llvmlibc-*,-readability-magic-numbers,-hicpp-signed-bitwise,-readability-function-cognitive-complexity,-google-objc-*,-hicpp-avoid-c-arrays,-hicpp-no-array-decay,-fuchsia-*,-cppcoreguidelines-owning-memory,-google-build-using-namespace,-modernize-avoid-c-arrays,-cppcoreguidelines-pro-bounds-array-to-pointer-decay,-cppcoreguidelines-avoid-c-arrays,-cppcoreguidelines-avoid-non-const-global-variables,-performance-no-int-to-ptr,-cert-dc*,-bugprone-reserved-identifier,-hicpp-no-assembler,-readability-isolate-declaration,-cppcoreguidelines-init-variables,-readability-identifier-length,-bugprone-easily-swappable-parameters,-cert-err33-c,-misc-non-private-member-variables-in-classes"
+    HeaderFilterRegex: ""
+  '';
 }
