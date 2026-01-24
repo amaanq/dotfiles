6a1a2acad0c39ff70b0a627fd5468e332982a6fe
Amaan Qureshi <git@amaanq.com>
2026-01-24 01:25:53 -0500

    mpv: unredeem hm and stylix

--- modules/linux/mpv.nix
+++ modules/linux/mpv.nix
@@ -1,12 +1,25 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
-  inherit (lib) enabled merge mkIf;
+  colors = lib.theme.withHashtag;
 in
-merge
-<| mkIf config.isDesktop {
-  home-manager.sharedModules = [
-    {
-      programs.mpv = enabled;
-    }
-  ];
+lib.mkIf config.isDesktop {
+  environment.systemPackages = [ pkgs.mpv ];
+
+  environment.variables.MPV_HOME = "/etc/mpv";
+
+  environment.etc."mpv/mpv.conf".text = ''
+    # Rose Pine OSD theme
+    background-color=#000000
+    osd-back-color=${colors.base01}
+    osd-border-color=${colors.base01}
+    osd-color=${colors.base05}
+    osd-shadow-color=${colors.base00}
+    osd-font=${config.theme.font.sans.name}
+    sub-font=${config.theme.font.sans.name}
+  '';
 }
