2f856bb243610c080f30bb4baf27d15c094708d4
Amaan Qureshi <git@amaanq.com>
2026-01-25 17:11:41 -0500

    discord: remove krisp patcher

--- modules/common/discord.nix
+++ modules/common/discord.nix
@@ -1,9 +1,4 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+{ config, lib, ... }:
 let
   inherit (lib)
     merge
@@ -267,36 +262,4 @@ in
 merge
 <| mkIf config.isDesktop {
   environment.etc."themes/discord.css".text = discordCss;
-
-  environment.systemPackages =
-    let
-      krisp-patcher =
-        pkgs.writers.writePython3Bin "krisp-patcher"
-          {
-            libraries = [
-              pkgs.python3Packages.capstone
-              pkgs.python3Packages.pyelftools
-            ];
-            flakeIgnore = [
-              "E501" # line too long (82 > 79 characters)
-              "F403" # 'from module import *' used; unable to detect undefined names
-              "F405" # name may be undefined, or defined from star imports: module
-            ];
-          }
-          (
-            builtins.readFile (
-              pkgs.fetchurl {
-                url = "https://raw.githubusercontent.com/sersorrel/sys/afc85e6b249e5cd86a7bcf001b544019091b928c/hm/discord/krisp-patcher.py";
-                sha256 = "sha256-h8Jjd9ZQBjtO3xbnYuxUsDctGEMFUB5hzR/QOQ71j/E=";
-              }
-            )
-          );
-    in
-    [
-      krisp-patcher
-    ];
-
-  unfree.allowedNames = [
-    "discord"
-  ];
 }
