7868312fcbcfcda085aa772c43cb0dc7f6018fab
Amaan Qureshi <git@amaanq.com>
2026-01-24 20:28:44 -0500

    shell(darwin): unredeem hm for hushlogin

--- modules/darwin/stop-login-print-in-shell.nix
+++ modules/darwin/stop-login-print-in-shell.nix
@@ -1,7 +1,10 @@
 {
-  home-manager.sharedModules = [
-    {
-      home.file.".hushlogin".text = "";
-    }
-  ];
+  # Create .hushlogin for all users to suppress login message
+  system.activationScripts.postActivation.text = ''
+    for user_home in /Users/*; do
+      if [ -d "$user_home" ]; then
+        touch "$user_home/.hushlogin"
+      fi
+    done
+  '';
 }
