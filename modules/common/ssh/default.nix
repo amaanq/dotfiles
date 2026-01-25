22c65d60d81a6854eaea3cf6ab6e76c125298077
Amaan Qureshi <git@amaanq.com>
2026-01-24 19:52:20 -0500

    ssh: unredeem hm

--- modules/common/ssh/default.nix
+++ modules/common/ssh/default.nix
@@ -7,78 +7,67 @@
 }:
 let
   inherit (lib)
-    enabled
-    mkIf
+    concatStringsSep
     filterAttrs
     attrNames
-    mapAttrs
+    mapAttrsToList
     head
+    merge
+    mkIf
     remove
     ;
 
   controlPath = "~/.ssh/control";
 
+  # Generate host blocks from nixosConfigurations
   hosts =
     self.nixosConfigurations
     |> filterAttrs (_: value: value.config.services.openssh.enable)
-    |> mapAttrs (
-      _: value: {
+    |> mapAttrsToList (
+      name: value:
+      let
         user =
           value.config.users.users
-          |> filterAttrs (_: value: value.isNormalUser)
+          |> filterAttrs (_: u: u.isNormalUser)
           |> attrNames
           |> remove "backup"
           |> remove "build"
           |> remove "root"
           |> head;
-      }
+      in
+      ''
+        Host ${name}
+          User ${user}
+      ''
     );
+
+  sshConfig = ''
+    # Include secrets
+    Include ${config.secrets.sshConfig.path}
+
+    # Hosts from nixosConfigurations
+    ${concatStringsSep "\n" hosts}
+
+    # Default settings for all hosts
+    Host *
+      ControlMaster auto
+      ControlPath ${controlPath}/%r@%n:%p
+      ControlPersist 60m
+      ServerAliveCountMax 2
+      ServerAliveInterval 60
+      SetEnv COLORTERM="truecolor" TERM="xterm-256color"
+      IdentityFile ~/.ssh/id
+  '';
 in
-{
+merge {
   secrets.sshConfig = {
     file = ./config.age;
     mode = "444";
   };
 
-  home-manager.sharedModules = [
-    (
-      homeArgs:
-      let
-        lib' = homeArgs.lib;
-
-        inherit (lib'.hm.dag) entryAfter;
-      in
-      {
-        home.activation.createControlPath =
-          entryAfter [ "writeBoundary" ] # bash
-            ''
-              mkdir --parents ${controlPath}
-            '';
-
-        programs.ssh = enabled {
-          enableDefaultConfig = false;
-          includes = [ config.secrets.sshConfig.path ];
-          matchBlocks = hosts // {
-            "*" = {
-              controlMaster = "auto";
-              controlPath = "${controlPath}/%r@%n:%p";
-              controlPersist = "60m";
-              serverAliveCountMax = 2;
-              serverAliveInterval = 60;
-
-              setEnv.COLORTERM = "truecolor";
-              setEnv.TERM = "xterm-256color";
-
-              identityFile = "~/.ssh/id";
-            };
-          };
-        };
-      }
-    )
-  ];
-
-  environment = mkIf config.isDesktop {
-    systemPackages = [ pkgs.mosh ];
-    shellAliases.mosh = "mosh --no-init --no-ssh-pty";
-  };
+  programs.ssh.extraConfig = sshConfig;
+}
+<| mkIf config.isDesktop {
+  environment.systemPackages = [ pkgs.mosh ];
+  environment.shellAliases.mosh = "mosh --no-init --no-ssh-pty";
 }
