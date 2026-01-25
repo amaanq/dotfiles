d956024272dab6ea2f4596ddf9c9f675c02737c7
Amaan Qureshi <git@amaanq.com>
2026-01-25 00:17:30 -0500

    darwin: remove spotify override

--- modules/common/spicetify.nix
+++ modules/common/spicetify.nix
@@ -63,22 +63,4 @@ merge
     // optionalAttrs config.isLinux {
       wayland = true;
     };
-
-  nixpkgs.overlays = optionals config.isDarwin [
-    (final: prev: {
-      spotify = prev.spotify.overrideAttrs (oldAttrs: {
-        src =
-          if prev.stdenv.isAarch64 then
-            prev.fetchurl {
-              url = "https://web.archive.org/web/20251010104459/https://download.scdn.co/SpotifyARM64.dmg";
-              hash = "sha256-0gwoptqLBJBM0qJQ+dGAZdCD6WXzDJEs0BfOxz7f2nQ=";
-            }
-          else
-            prev.fetchurl {
-              url = "https://web.archive.org/web/20251010104433/https://download.scdn.co/Spotify.dmg";
-              hash = "sha256-8CrhLbnswbuAjRMaan2cTnnOMsr3vpW92IQ00KwPUHo=";
-            };
-      });
-    })
-  ];
 }
