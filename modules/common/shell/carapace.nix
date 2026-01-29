45afded92adabfa1d0396ebe0ed4fcbd908f8462
Amaan Qureshi <git@amaanq.com>
2026-01-29 16:20:58 -0500

    carapace: drop custom patch

--- modules/common/shell/carapace.nix
+++ modules/common/shell/carapace.nix
@@ -1,11 +1,5 @@
 { pkgs, ... }:
 let
-  carapace = pkgs.carapace.overrideAttrs (old: {
-    patches = (old.patches or [ ]) ++ [
-      ./carapace-adb-devices.patch
-    ];
-  });
-
   # node-pty native addon is incompatible with Node.js 24's V8 API changes
   inshellisense = pkgs.inshellisense.override {
     buildNpmPackage = pkgs.buildNpmPackage.override { nodejs = pkgs.nodejs_22; };
@@ -13,7 +7,7 @@ let
 in
 {
   environment.systemPackages = [
-    carapace
+    pkgs.carapace
     pkgs.fish
     pkgs.zsh
     inshellisense
