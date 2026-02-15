e493b3e6a4f95fd2465c7626e9353ab0a117d346
Amaan Qureshi <git@amaanq.com>
2026-02-15 06:51:41 -0500

    niri: more preset column widths

--- modules/linux/niri/default.nix
+++ modules/linux/niri/default.nix
@@ -188,8 +188,12 @@ in
           inactive.color = "#${theme.base03}";
         };
         preset-column-widths = [
+          { proportion = 1. / 4.; }
           { proportion = 1. / 3.; }
+          { proportion = 1. / 2.; }
           { proportion = 2. / 3.; }
+          { proportion = 3. / 4.; }
+          { proportion = 1.; }
         ];
         background-color = "transparent";
       };
