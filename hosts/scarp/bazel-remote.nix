3773c907c1c4679245d15eab30d45c517ab10f7a
Amaan Qureshi <git@amaanq.com>
2026-02-16 22:13:37 -0500

    bazel-remote: expose bazel cache on scarp via nginx

--- hosts/scarp/bazel-remote.nix
+++ hosts/scarp/bazel-remote.nix
@@ -1,5 +1,17 @@
-{ pkgs, ... }:
 {
+  self,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  inherit (config.networking) domain;
+  inherit (lib) merge;
+in
+{
+  imports = [ (self + /modules/nginx.nix) ];
+
   # RBE-compatible cache backend for Android builds (reclient)
   # Provides CAS + Action Cache over gRPC on port 9092
   systemd.services.bazel-remote = {
@@ -8,10 +20,18 @@
     wantedBy = [ "multi-user.target" ];
 
     serviceConfig = {
-      ExecStart = "${pkgs.bazel-remote}/bin/bazel-remote --dir /var/cache/bazel-remote --max_size 50 --enable_ac_key_instance_mangling --grpc_address 0.0.0.0:9092 --http_address 0.0.0.0:9091";
+      ExecStart = "${pkgs.bazel-remote}/bin/bazel-remote --dir /var/cache/bazel-remote --max_size 50 --enable_ac_key_instance_mangling --grpc_address 127.0.0.1:9092 --http_address 127.0.0.1:9091";
       User = "bazel-remote";
       Group = "bazel-remote";
       CacheDirectory = "bazel-remote";
+    };
+  };
+
+  services.nginx.virtualHosts."android.${domain}" = merge config.services.nginx.sslTemplate {
+    locations."/" = {
+      extraConfig = ''
+        grpc_pass grpc://127.0.0.1:9092;
+      '';
     };
   };
 
@@ -19,5 +39,5 @@
     isSystemUser = true;
     group = "bazel-remote";
   };
-  users.groups.bazel-remote = {};
+  users.groups.bazel-remote = { };
 }
