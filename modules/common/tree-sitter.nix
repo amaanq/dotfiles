e9f30c593a094d25eb5e99f34d7b064f9f0c32a2
Amaan Qureshi <git@amaanq.com>
2026-02-27 04:22:54 -0500

    tree-sitter: bump to 0.26.6

--- modules/common/tree-sitter.nix
+++ modules/common/tree-sitter.nix
@@ -1,4 +1,26 @@
 { pkgs, ... }:
+let
+  tree-sitter = pkgs.tree-sitter.overrideAttrs (old: rec {
+    version = "0.26.6";
+    src = pkgs.fetchFromGitHub {
+      owner = "tree-sitter";
+      repo = "tree-sitter";
+      tag = "v${version}";
+      hash = "sha256-ZtzwhEmNZg5brghKNiTRZSmY8FwQeWcemY2blq9j2GM=";
+      fetchSubmodules = true;
+    };
+    cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
+      inherit src;
+      hash = "sha256-u6RmwNR4QVwyuij5RlHTLC5lNNQpWMVrlQwfwF78pYc=";
+    };
+    nativeBuildInputs = old.nativeBuildInputs ++ [ pkgs.libclang ];
+    env = (old.env or { }) // {
+      LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+      BINDGEN_EXTRA_CLANG_ARGS = "-isystem ${pkgs.stdenv.cc.libc.dev}/include";
+    };
+    patches = [ ];
+  });
+in
 {
   environment.shellAliases = {
     ts = "tree-sitter";
@@ -6,7 +28,7 @@
     tss = "tree-sitter-stable";
   };
   environment.systemPackages = [
-    pkgs.tree-sitter
+    tree-sitter
     pkgs.python313Packages.pytest
   ];
 }
