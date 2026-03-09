7e710cf42b2b8835f3a30587aab2e558d60aff5d
Amaan Qureshi <git@amaanq.com>
2026-03-08 20:11:55 -0400

    esker: add power optimizations and Framework EC support

--- hosts/esker/hardware.nix
+++ hosts/esker/hardware.nix
@@ -22,7 +22,22 @@ in
   ];
   boot.initrd.kernelModules = [ "amdgpu" ];
   boot.kernelModules = [ "kvm-amd" ];
-  boot.extraModulePackages = [ ];
+  boot.extraModulePackages = [ config.boot.kernelPackages.framework-laptop-kmod ];
+
+  bunker.kernel.framework = true;
+
+  # Fix PSR hangs and graphics issues on AMD AI 300 series.
+  # https://gist.github.com/lbrame/f9034b1a9fe4fc2d2835c5542acb170a
+  boot.kernelParams = [
+    "amdgpu.dcdebugmask=0x410"
+    "amdgpu.sg_display=0"
+    "amdgpu.abmlevel=0"
+  ];
+
+  # Ethernet expansion card USB autosuspend.
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="0bda", ATTR{idProduct}=="8156", ATTR{power/autosuspend}="20"
+  '';
 
   boot.loader.systemd-boot = enabled;
   boot.loader.efi.canTouchEfiVariables = true;
--- modules/linux/desktop/power.nix
+++ modules/linux/desktop/power.nix
@@ -24,5 +24,20 @@ in
 
     boot.kernelModules = [ "acpi_call" ];
     boot.extraModulePackages = [ config.boot.kernelPackages.acpi_call ];
+
+    # Use the most aggressive PCIe ASPM policy for idle power savings.
+    boot.kernelParams = [ "pcie_aspm.policy=powersupersave" ];
+
+    services.udev.extraRules = ''
+      # Enable PCI runtime power management on all devices.
+      ACTION=="add", SUBSYSTEM=="pci", ATTR{power/control}="auto"
+
+      # Limit battery charge to 80% to preserve long-term health.
+      # Charging resumes at 75% to avoid constant cycling.
+      ACTION=="change", SUBSYSTEM=="power_supply", KERNEL=="BAT*", ATTR{charge_control_end_threshold}="80"
+      ACTION=="add", SUBSYSTEM=="power_supply", KERNEL=="BAT*", ATTR{charge_control_end_threshold}="80"
+      ACTION=="change", SUBSYSTEM=="power_supply", KERNEL=="BAT*", ATTR{charge_control_start_threshold}="75"
+      ACTION=="add", SUBSYSTEM=="power_supply", KERNEL=="BAT*", ATTR{charge_control_start_threshold}="75"
+    '';
   };
 }
