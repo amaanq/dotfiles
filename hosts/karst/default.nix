7487afb628f498a79681da5611e4114a6cc09810
Amaan Qureshi <git@amaanq.com>
2026-03-18 13:47:04 -0400

    hosts: init karst and loess
    Thank you to the OSUOSL!

---
+++ hosts/karst/default.nix
@@ -0,0 +1,65 @@
+lib:
+lib.nixosSystem' "server" (
+  {
+    keys,
+    pkgs,
+    ...
+  }:
+  let
+    inherit (lib) collectNix remove;
+  in
+  {
+    imports = collectNix ./. |> remove ./default.nix;
+
+    type = "server";
+
+    services.openssh.settings = {
+      PermitRootLogin = "yes";
+      PasswordAuthentication = false;
+      PrintLastLog = false;
+    };
+
+    users.users = {
+      root = {
+        openssh.authorizedKeys.keys = keys.admins;
+        shell = pkgs.nushell;
+      };
+
+      amaanq = {
+        description = "Amaan Qureshi";
+        isNormalUser = true;
+        extraGroups = [ "wheel" ];
+        openssh.authorizedKeys.keys = keys.admins;
+        shell = pkgs.nushell;
+      };
+
+      max = {
+        isNormalUser = true;
+        openssh.authorizedKeys.keys = [
+          "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIHynM2pDVAe8ZlooMYFBTAhoEW1lV066GtoxjJJ0qEs6AAAAB3NzaDptYXg= max@privatevoid.net"
+        ];
+      };
+
+      lillis = {
+        isNormalUser = true;
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIYWWRfOsSpi7M6ejCEWHGTtsvOA8v7FiUOBR2If1nVa will.lillis24@gmail.com"
+        ];
+      };
+    };
+
+    networking = {
+      hostName = "karst";
+      useDHCP = true;
+    };
+
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+    boot.supportedFilesystems = [ "bcachefs" ];
+    boot.tmp.cleanOnBoot = true;
+
+    system.stateVersion = "25.11";
+
+    time.timeZone = "America/Los_Angeles";
+  }
+)
---
+++ hosts/loess/disko.nix
@@ -0,0 +1,42 @@
+{
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/vda";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [
+                  "fmask=0077"
+                  "dmask=0077"
+                ];
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "bcachefs";
+                mountpoint = "/";
+                mountOptions = [ "noatime" ];
+                extraArgs = [
+                  "--block_size=16384"
+                  "--compression=zstd:9"
+                  "--background_compression=zstd:9"
+                ];
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
