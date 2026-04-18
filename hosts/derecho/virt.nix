2a499e9edb7f6d1d5548b971fbd3b2fde0903ccd
Amaan Qureshi <git@amaanq.com>
2026-04-18 19:41:38 -0400

    virt: go serial-only
    virt-manager drags gtksourceview-4 for its XML panel and virt-viewer drags
    the full spice-gtk stack for a console window. My only guests are NixOS and
    illumos, both happy on ttyS0. virt-install creates, virsh edit rewrites
    XML, virsh console attaches the getty. SPICE USB redirection and
    spice-vdagent go with it since nothing graphical remains.

--- hosts/derecho/virt.nix
+++ hosts/derecho/virt.nix
@@ -9,14 +9,6 @@ let
 in
 merge
 <| mkIf config.isDesktop {
-  environment.systemPackages = [
-    pkgs.virt-viewer
-  ];
-
-  programs.virt-manager = enabled;
-
-  services.spice-vdagentd = enabled;
-
   users.users.amaanq = {
     extraGroups = [
       "libvirtd"
@@ -24,17 +16,13 @@ merge
     ];
   };
 
-  virtualisation = {
-    libvirtd = enabled {
-      qemu = {
-        package = pkgs.qemu;
-        swtpm = enabled;
-        vhostUserPackages = [ pkgs.virtiofsd ];
-      };
-      onBoot = "ignore";
-      onShutdown = "shutdown";
+  virtualisation.libvirtd = enabled {
+    qemu = {
+      package = pkgs.qemu;
+      swtpm = enabled;
+      vhostUserPackages = [ pkgs.virtiofsd ];
     };
-
-    spiceUSBRedirection = enabled;
+    onBoot = "ignore";
+    onShutdown = "shutdown";
   };
 }
