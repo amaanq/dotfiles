0809c979120fe9fbc9fc0cac63c033660a0eb221
Amaan Qureshi <git@amaanq.com>
2026-04-18 20:52:27 -0400

    desktop: drop rose-pine-gtk theme
    Rose-pine-gtk dragged gtk-engine-murrine and gtk+2 in just to paint GTK2
    apps I don't run. Remaining GTK apps are Chromium/Helium file pickers and
    mate-polkit dialogs, both happy with Adwaita. Kept the dconf color-scheme
    = prefer-dark plus gtk-application-prefer-dark-theme so they still render
    dark; ditched the GTK_THEME env var and the explicit theme names so GTK
    falls through to its default.

--- modules/linux/desktop/gtk.nix
+++ modules/linux/desktop/gtk.nix
@@ -1,19 +1,15 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }:
 let
   inherit (lib) enabled;
 in
 {
-  environment.systemPackages = [ pkgs.rose-pine-gtk-theme ];
-
-  # GTK3 settings
   environment.etc."xdg/gtk-3.0/settings.ini".text = ''
     [Settings]
-    gtk-theme-name=rose-pine
+    gtk-application-prefer-dark-theme=1
     gtk-icon-theme-name=${config.theme.icons.name}
     gtk-font-name=${config.theme.font.sans.name} ${toString config.theme.font.size.normal}
     gtk-xft-antialias=1
@@ -28,25 +24,17 @@ in
     gtk-enable-input-feedback-sounds=0
   '';
 
-  # GTK4 settings
   environment.etc."xdg/gtk-4.0/settings.ini".text = ''
     [Settings]
-    gtk-theme-name=rose-pine
     gtk-icon-theme-name=${config.theme.icons.name}
     gtk-font-name=${config.theme.font.sans.name} ${toString config.theme.font.size.normal}
   '';
 
-  environment.variables = {
-    GTK_THEME = "rose-pine";
-  };
-
-  # dconf settings for GNOME/GTK apps
   programs.dconf = enabled {
     profiles.user.databases = [
       {
         settings = {
           "org/gnome/desktop/interface" = {
-            gtk-theme = "rose-pine";
             icon-theme = config.theme.icons.name;
             font-name = "${config.theme.font.sans.name} ${toString config.theme.font.size.normal}";
             color-scheme = "prefer-dark";
