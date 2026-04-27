c4048d33604b855a76b506b47da43903605bef25
Amaan Qureshi <git@amaanq.com>
2026-04-26 21:33:11 -0400

    perl-free: package nixos-core via inputs callPackage, disable command-not-found

--- modules/linux/perl-free.nix
+++ modules/linux/perl-free.nix
@@ -1,4 +1,9 @@
-{ lib, ... }:
+{
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
 let
   inherit (lib) enabled;
 in
@@ -8,11 +13,17 @@ in
   #   setup-etc.pl                (/etc/static atomic update)
   #   stage-1-init.sh             (legacy initrd boot)
   #   stage-2-init.sh             (system activation post-initrd)
-  system.nixos-core = enabled;
+  system.nixos-core = enabled {
+    package = pkgs.callPackage (inputs.nixos-core + "/nix/package.nix") { };
+  };
+
 
   # We use flakes anyway, so get rid of this perl slop.
   system.tools.nixos-generate-config.enable = lib.mkDefault false;
 
   # I'm never allowing perl on my system again.
   system.forbiddenDependenciesRegexes = [ "[^[:alpha:]][Pp]erl" ];
+
+  # For some reason, this is in Perl.
+  programs.command-not-found.enable = false;
 }
