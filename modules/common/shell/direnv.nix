055dc5c8028032f5be442fd126d513f1c29f8623
Amaan Qureshi <git@amaanq.com>
2026-04-26 21:33:11 -0400

    direnv: enable only on desktops

--- modules/common/shell/direnv.nix
+++ modules/common/shell/direnv.nix
@@ -1,9 +1,11 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }:
 {
   environment.systemPackages = [
     pkgs.direnv
-    pkgs.nix-direnv
-  ];
+  ]
+  ++ lib.optional config.isDesktop pkgs.nix-direnv;
 
-  environment.etc."direnv/lib/nix-direnv.sh".source = "${pkgs.nix-direnv}/share/nix-direnv/direnvrc";
+  environment.etc = lib.mkIf config.isDesktop {
+    "direnv/lib/nix-direnv.sh".source = "${pkgs.nix-direnv}/share/nix-direnv/direnvrc";
+  };
 }
