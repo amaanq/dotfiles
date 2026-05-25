a424f236ed91cc71f3ae4696c00fdd96f18e0a9d
Amaan Qureshi <git@amaanq.com>
2026-05-24 20:45:40 -0400

    nativelink: put android and chromium on separate instances

--- hosts/scarp/nativelink/default.nix
+++ hosts/scarp/nativelink/default.nix
@@ -1,66 +1,88 @@
-{ pkgs, nativelink, ... }:
+{
+  lib,
+  pkgs,
+  nativelink,
+  ...
+}:
 let
+  inherit (lib) concatLists zipAttrs;
+
   gib = 1024 * 1024 * 1024;
 
+  mkInstance =
+    name:
+    let
+      cas = {
+        instance_name = name;
+        cas_store = "${name}-cas";
+      };
+    in
+    {
+      stores = [
+        {
+          name = "${name}-cas-fs";
+          filesystem = {
+            content_path = "/var/cache/nativelink/${name}/cas";
+            temp_path = "/var/cache/nativelink/${name}/cas-tmp";
+            eviction_policy.max_bytes = 150 * gib;
+          };
+        }
+        {
+          # size+hash verification on write keeps the CAS honest
+          name = "${name}-cas";
+          verify = {
+            backend.ref_store.name = "${name}-cas-fs";
+            verify_size = true;
+            verify_hash = true;
+          };
+        }
+        {
+          name = "${name}-ac-fs";
+          filesystem = {
+            content_path = "/var/cache/nativelink/${name}/ac";
+            temp_path = "/var/cache/nativelink/${name}/ac-tmp";
+            eviction_policy.max_bytes = 10 * gib;
+          };
+        }
+        {
+          name = "${name}-ac";
+          completeness_checking = {
+            backend.ref_store.name = "${name}-ac-fs";
+            cas_store.ref_store.name = "${name}-cas";
+          };
+        }
+      ];
+      inherit cas;
+      bytestream = cas;
+      ac = {
+        instance_name = name;
+        ac_store = "${name}-ac";
+      };
+      capabilities = {
+        instance_name = name;
+      };
+    };
+
+  instances = [
+    "android"
+    "chromium"
+  ];
+
+  perInstance = zipAttrs (map mkInstance instances);
+
   nativelinkConfig = (pkgs.formats.json { }).generate "nativelink.json5" {
-    stores = [
-      {
-        name = "CAS_FS";
-        filesystem = {
-          content_path = "/var/cache/nativelink/cas";
-          temp_path = "/var/cache/nativelink/cas-tmp";
-          eviction_policy.max_bytes = 150 * gib;
-        };
-      }
-      {
-        # size+hash verification on write keeps the CAS honest
-        name = "CAS_MAIN";
-        verify = {
-          backend.ref_store.name = "CAS_FS";
-          verify_size = true;
-          verify_hash = true;
-        };
-      }
-      {
-        name = "AC_MAIN";
-        filesystem = {
-          content_path = "/var/cache/nativelink/ac";
-          temp_path = "/var/cache/nativelink/ac-tmp";
-          eviction_policy.max_bytes = 10 * gib;
-        };
-      }
-      {
-        name = "AC_CHECKED";
-        completeness_checking = {
-          backend.ref_store.name = "AC_MAIN";
-          cas_store.ref_store.name = "CAS_MAIN";
-        };
-      }
-    ];
+    stores = concatLists perInstance.stores;
     servers = [
       {
         listener.http.socket_address = "100.64.0.2:50051";
         listener.http.freebind = true;
         services = {
-          cas = [
-            {
-              instance_name = "main";
-              cas_store = "CAS_MAIN";
-            }
-          ];
-          ac = [
-            {
-              instance_name = "main";
-              ac_store = "AC_CHECKED";
-            }
-          ];
-          capabilities = [ { instance_name = "main"; } ];
-          bytestream = [
-            {
-              instance_name = "main";
-              cas_store = "CAS_MAIN";
-            }
-          ];
+          inherit (perInstance)
+            cas
+            ac
+            capabilities
+            bytestream
+            ;
         };
       }
     ];
