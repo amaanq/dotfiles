51cf123e157dfef37417df74cb13b34b52cfbde5
Amaan Qureshi <git@amaanq.com>
2026-05-24 20:45:40 -0400

    nixos-{option,rebuild}: disable

--- modules/linux/perl-free.nix
+++ modules/linux/perl-free.nix
@@ -17,9 +17,10 @@ in
     package = pkgs.callPackage (inputs.nixos-core + "/nix/package.nix") { };
   };
 
-
-  # We use flakes anyway, so get rid of this perl slop.
+  # We use flakes anyway, so get rid of this perl or perl-dependent slop.
   system.tools.nixos-generate-config.enable = lib.mkDefault false;
+  system.tools.nixos-option.enable = lib.mkDefault false;
+  system.tools.nixos-rebuild.enable = lib.mkDefault false;
 
   # I'm never allowing perl on my system again.
   system.forbiddenDependenciesRegexes = [ "[^[:alpha:]][Pp]erl" ];
